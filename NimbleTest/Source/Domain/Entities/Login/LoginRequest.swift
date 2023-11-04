4d271f88bfa3e3b2ae469aa7b886c6bc5a39cc61
Joan Wilson <joanwmoliveira@gmail.com>
2023-11-04 16:58:03 -0300

    ♻️refactor: remove comments in LoginRequest File

--- NimbleTest/Source/Domain/Entities/Login/LoginRequest.swift
+++ NimbleTest/Source/Domain/Entities/Login/LoginRequest.swift
@@ -1,26 +1,5 @@
 import Foundation
 
-//struct LoginRequest {
-//    let grantType: String
-//    let email: String
-//    let password: String
-//
-//    var dictionaryParameters: [String: Any] {
-//        guard let clientId = Bundle.main.infoDictionary?["CLIENT_ID"] as? String,
-//              let clientSecret = Bundle.main.infoDictionary?["CLIENT_SECRET"] as? String else {
-//            return [:]
-//        }
-//
-//        return [
-//            "grant_type": "password",
-//            "email": email,
-//            "password": password,
-//            "client_id": clientId,
-//            "client_secret": clientSecret
-//        ]
-//    }
-//}
-
 struct LoginRequest: Codable {
     let grantType, email, password, clientID: String
     let clientSecret: String
